03587b0abc076d0dd0cc5ca8561f8576513e5e8a
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-05-03 17:21:56 +0200

    Pbt: add neutrals propreties

--- src/pbt/properties.ml
+++ src/pbt/properties.ml
@@ -1,2 +1,8 @@
 (* Properties *)
 let commutative f x y = f x y = f y x
+
+let neutral_left f neutral x = f neutral x = x
+
+let neutral_right f neutral x = f x neutral = x
+
+let neutrals f neutral x = neutral_right f neutral x && neutral_left f neutral x
