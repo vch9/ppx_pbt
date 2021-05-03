c0bd31a35855077038676de13f84333d43818f84
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-05-03 11:59:15 +0200

    Ppx: improve errors on properties

--- src/ppx/error.ml
+++ src/ppx/error.ml
@@ -4,9 +4,21 @@ exception SyntaxError of string
 
 exception CaseUnsupported of string
 
+exception PropertyNotSupported of string
+
+exception PropertyGeneratorsMissing of string * int * int
+
 let syntax_error c = raise (SyntaxError (Format.sprintf "%c" c))
 
 let print_exception = function
   | SyntaxError s -> Format.printf "SyntaxError (%s)\n" s
   | CaseUnsupported s -> Format.printf "CaseUnsupported in (%s)\n" s
+  | PropertyNotSupported s ->
+      Format.printf "The property %s is not supported in ppx_pbt\n" s
+  | PropertyGeneratorsMissing (s, actual, expected) ->
+      Format.printf
+        "Property %s requires %d generators, here %d are applied\n"
+        s
+        actual
+        expected
   | e -> Format.printf "InternalError (%s)\n" (Printexc.to_string e)
