2a984dddb7266bf753f2d5768d78bbccd4bbd934
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-05-04 15:19:09 +0200

    Pbt: associative property

--- src/pbt/properties.ml
+++ src/pbt/properties.ml
@@ -1,6 +1,8 @@
 (* Properties *)
 let commutative f x y = f x y = f y x
 
+let associative f x y z = f (f x y) z = f x (f y z)
+
 let neutral_left f neutral x = f neutral x = x
 
 let neutral_right f neutral x = f x neutral = x
