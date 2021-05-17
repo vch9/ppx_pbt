6740f6989fa5b410fa6a0a775a9584c32985a019
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-05-17 10:25:43 +0200

    Testing: gen generator test

---
+++ test/rewriter/gen/test_primitive.expected.ml
@@ -0,0 +1,23 @@
+type t1 = int [@@gen]
+
+let gen_t1 = Pbt.Gens.int
+
+type t2 = float [@@gen]
+
+let gen_t2 = Pbt.Gens.float
+
+type t3 = char [@@gen]
+
+let gen_t3 = Pbt.Gens.char
+
+type t4 = string [@@gen]
+
+let gen_t4 = Pbt.Gens.string
+
+type t5 = unit [@@gen]
+
+let gen_t5 = Pbt.Gens.unit
+
+type t6 = bool [@@gen]
+
+let gen_t6 = Pbt.Gens.bool
---
+++ test/rewriter/gen/test_primitive.ml
@@ -0,0 +1,11 @@
+type t1 = int [@@gen]
+
+type t2 = float [@@gen]
+
+type t3 = char [@@gen]
+
+type t4 = string [@@gen]
+
+type t5 = unit [@@gen]
+
+type t6 = bool [@@gen]
