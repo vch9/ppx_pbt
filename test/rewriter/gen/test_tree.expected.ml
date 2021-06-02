3d6336cf8f9f5e305e4d86cc84943ccdac2f7b22
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-06-02 15:21:32 +0200

    Use mutual recursive auxiliar functions

--- test/rewriter/gen/test_tree.expected.ml
+++ test/rewriter/gen/test_tree.expected.ml
@@ -1,8 +1,10 @@
 include struct
   type tree = Leaf | Node of int * tree * tree [@@gen]
 
-  let gen_tree =
-    let rec gen_tree' = function
+  include struct
+    let rec gen_tree () = gen_tree' 5
+
+    and gen_tree' = function
       | 0 -> QCheck.oneof [ QCheck.make @@ QCheck.Gen.return Leaf ]
       | n ->
           QCheck.oneof
@@ -14,8 +16,9 @@ include struct
                    QCheck.int
                    (QCheck.pair (gen_tree' (n - 1)) (gen_tree' (n - 1))));
             ]
-    in
-    gen_tree' 5
+
+    let gen_tree = gen_tree ()
+  end
 end
 
 include struct
@@ -26,8 +29,10 @@ include struct
     | Lt of expr * expr
   [@@gen]
 
-  let gen_expr =
-    let rec gen_expr' = function
+  include struct
+    let rec gen_expr () = gen_expr' 5
+
+    and gen_expr' = function
       | 0 -> QCheck.oneof [ QCheck.map (fun gen_0 -> Value gen_0) QCheck.int ]
       | n ->
           QCheck.oneof
@@ -45,7 +50,7 @@ include struct
                 (fun (gen_0, gen_1) -> Lt (gen_0, gen_1))
                 (QCheck.pair (gen_expr' (n - 1)) (gen_expr' (n - 1)));
             ]
-    in
 
-    gen_expr' 5
+    let gen_expr = gen_expr ()
+  end
 end
